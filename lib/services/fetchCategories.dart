012c98f21e23b30450e9421b4eea539f6e78d01e
Onwuzor Victor <onwuzorvictor@outlook.com>
2020-08-10 17:33:32 +0100

    add product and category service

--- lib/services/fetchCategories.dart
+++ lib/services/fetchCategories.dart
@@ -1,23 +1,23 @@
 import 'dart:convert';
 
-import 'package:furniture/models/Product.dart';
+import 'package:furniture/models/Categories.dart';
 import 'package:http/http.dart' as http;
 
-// Fetch our Products from API
-Future<List<Product>> fetchProducts() async {
+// Fetch our Categories from API
+Future<List<Category>> fetchCategories() async {
   const String apiUrl =
-      "https://5f210aa9daa42f001666535e.mockapi.io/api/products";
+      "https://5f210aa9daa42f001666535e.mockapi.io/api/categories";
 
   final response = await http.get(apiUrl);
 
   if (response.statusCode == 200) {
     // If the server did return a 200 OK response,
     // then parse the JSON.
-    List<Product> products = (json.decode(response.body) as List)
-        .map((data) => Product.fromJson(data))
+    List<Category> categories = (json.decode(response.body) as List)
+        .map((data) => Category.fromJson(data))
         .toList();
-    // Return list of products
-    return products;
+// It retuen list of categories
+    return categories;
   } else {
     // If the server did not return a 200 OK response,
     // then throw an exception.
